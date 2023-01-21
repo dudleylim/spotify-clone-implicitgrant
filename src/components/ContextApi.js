8885cb31fe5de8f0e5833efdb57dd6de3c9b5788
dudleylim <dudleylim@gmail.com>
2023-01-21 18:45:33 +0800

    changed code spacing

--- src/components/ContextApi.js
+++ src/components/ContextApi.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Context = React.createContext();
 export default Context;
@@ -8,143 +8,143 @@ export default Context;
 export const ContextApi = ({children}) => {
 // variables, states, functions
     // code for authorization
-    const CLIENT_ID = 'e3b1df6e525e43a78f45aae2537a6534';
-    const REDIRECT_URI = 'http://localhost:3000/';
-    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
-    const SCOPE = 'user-read-email streaming user-read-private user-read-playback-state user-modify-playback-state';
+        const CLIENT_ID = 'e3b1df6e525e43a78f45aae2537a6534';
+        const REDIRECT_URI = 'http://localhost:3000/';
+        const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+        const SCOPE = 'user-read-email streaming user-read-private user-read-playback-state user-modify-playback-state';
 
-    const [token, setToken] = useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+        const [token, setToken] = useState("");
+        const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        let urlParams = new URLSearchParams(window.location.hash.replace("#", "?"));
-        let token = window.localStorage.getItem("token");
-        // const navigate = useNavigate()
+        useEffect(() => {
+            let urlParams = new URLSearchParams(window.location.hash.replace("#", "?"));
+            let token = window.localStorage.getItem("token");
+            // const navigate = useNavigate()
 
-        if (!token && urlParams) {
-            try {
-                token = urlParams.get('access_token');
-                setToken(token);
-                console.log(urlParams);
-                // navigate('/');
-            } catch (error) {
-                console.log(error)
+            if (!token && urlParams) {
+                try {
+                    token = urlParams.get('access_token');
+                    setToken(token);
+                    console.log(urlParams);
+                    // navigate('/');
+                } catch (error) {
+                    console.log(error)
+                }
+
             }
-            
-        }
-    }, []);
+        }, []);
 
     // playback sdk
-    const [player, setPlayer] = useState({});
-    const [isSongReady, setIsSongReady] = useState(false);
-    const [isPlayerReady, setIsPlayerReady] = useState(false);
+        const [player, setPlayer] = useState({});
+        const [isSongReady, setIsSongReady] = useState(false);
+        const [isPlayerReady, setIsPlayerReady] = useState(false);
 
     // https://developer.spotify.com/documentation/web-playback-sdk/guide/
-    useEffect(() => {
-        console.log(token);
-        if (token !== "" && token !== null) {
+        useEffect(() => {
             console.log(token);
-            const script = document.createElement("script");
-            script.src = "https://sdk.scdn.co/spotify-player.js";
-            script.async = true;
-    
-            document.body.appendChild(script);
-    
-            window.onSpotifyWebPlaybackSDKReady = () => {
-    
-                const player = new window.Spotify.Player({
-                    name: 'Web Playback SDK',
-                    getOAuthToken: callback => { callback(token); },
-                    volume: 0.5
-                });
+            if (token !== "" && token !== null) {
+                console.log(token);
+                const script = document.createElement("script");
+                script.src = "https://sdk.scdn.co/spotify-player.js";
+                script.async = true;
+            
+                document.body.appendChild(script);
+            
+                window.onSpotifyWebPlaybackSDKReady = () => {
+                
+                    const player = new window.Spotify.Player({
+                        name: 'Web Playback SDK',
+                        getOAuthToken: callback => { callback(token); },
+                        volume: 0.5
+                    });
 
-                setPlayer(player);
-    
-                player.addListener('ready', ({ device_id }) => {
-                    console.log('Ready with Device ID', device_id);
-                    console.log(player);
+                    setPlayer(player);
+                
+                    player.addListener('ready', ({ device_id }) => {
+                        console.log('Ready with Device ID', device_id);
+                        console.log(player);
 
-                    // POSSIBLE POF
-                    // navigate('/');
-                    // POSSIBLE POF
-                });
-    
-                player.addListener('not_ready', ({ device_id }) => {
-                    console.log('Device ID has gone offline', device_id);
-                });
-    
-                player.addListener('initialization_error', ({ message }) => { 
-                    console.error(message);
-                });
-    
-                player.addListener('authentication_error', ({ message }) => {
-                    console.error(message);
-                });
-    
-                player.addListener('account_error', ({ message }) => {
-                    console.error(message);
-                });
+                        // POSSIBLE POF
+                        // navigate('/');
+                        // POSSIBLE POF
+                    });
+                
+                    player.addListener('not_ready', ({ device_id }) => {
+                        console.log('Device ID has gone offline', device_id);
+                    });
+                
+                    player.addListener('initialization_error', ({ message }) => { 
+                        console.error(message);
+                    });
+                
+                    player.addListener('authentication_error', ({ message }) => {
+                        console.error(message);
+                    });
+                
+                    player.addListener('account_error', ({ message }) => {
+                        console.error(message);
+                    });
 
-                player.addListener('autoplay_failed', () => {
-                    console.log('autoplay failed');
-                });
-    
-                player.addListener('player_state_changed', ({
-                        position,
-                        duration,
-                        track_window: { current_track }
-                    }) => {
-                        console.log('Currently Playing', current_track);
-                        setCurrentTrack(current_track);
-                        console.log('Position in Song', position);
-                        setUpdatedProgress(position);
-                        console.log('Duration of Song', duration);
-                        setCurrentDuration(duration);
+                    player.addListener('autoplay_failed', () => {
+                        console.log('autoplay failed');
+                    });
+                
+                    player.addListener('player_state_changed', ({
+                            position,
+                            duration,
+                            track_window: { current_track }
+                        }) => {
+                            console.log('Currently Playing', current_track);
+                            setCurrentTrack(current_track);
+                            console.log('Position in Song', position);
+                            setUpdatedProgress(position);
+                            console.log('Duration of Song', duration);
+                            setCurrentDuration(duration);
 
-                        setIsSongReady(true);
-                });
-    
-                player.connect().then(success => {
-                    if (success) {
-                        console.log('The Web Playback SDK successfully connected to Spotify!');
-                        setIsPlayerReady(true);
-                    } else {
-                        console.log('bruh')
-                    }
-                });
-    
-            };
-        }
-    }, [token])
+                            setIsSongReady(true);
+                    });
+                
+                    player.connect().then(success => {
+                        if (success) {
+                            console.log('The Web Playback SDK successfully connected to Spotify!');
+                            setIsPlayerReady(true);
+                        } else {
+                            console.log('bruh')
+                        }
+                    });
+                
+                };
+            }
+        }, [token])
 
     // playback states
-    const track = {
-        name: "",
-        album: {
-            images: [
-                { url: "" }
+        const track = {
+            name: "",
+            album: {
+                images: [
+                    { url: "" }
+                ]
+            },
+            artists: [
+                { name: "" }
             ]
-        },
-        artists: [
-            { name: "" }
-        ]
-    }
+        }
 
-    const [currentTrack, setCurrentTrack] = useState(track);
-    const [updatedProgress, setUpdatedProgress] = useState(0);
-    const [currentDuration, setCurrentDuration] = useState(0);
-    let durationSeconds = ((currentDuration % 60000) / 1000).toFixed(0);
-    let durationMinutes = Math.floor(currentDuration / 60000);
-    const [isPlaying, setIsPlaying] = useState(false);
+        const [currentTrack, setCurrentTrack] = useState(track);
+        const [updatedProgress, setUpdatedProgress] = useState(0);
+        const [currentDuration, setCurrentDuration] = useState(0);
+        let durationSeconds = ((currentDuration % 60000) / 1000).toFixed(0);
+        let durationMinutes = Math.floor(currentDuration / 60000);
+        const [isPlaying, setIsPlaying] = useState(false);
 
     
     // logout function
-    const logout = () => {
-        player.disconnect();
-        setToken("");
-        window.localStorage.removeItem("token");
-        // navigate('/');
-    }
+        const logout = () => {
+            player.disconnect();
+            setToken("");
+            window.localStorage.removeItem("token");
+            // navigate('/');
+        }
 
     // test function
     // const testing = () => {
